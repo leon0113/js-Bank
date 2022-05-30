55a73da62c0300371ec190a11f61f3e066166ea0
leon0113 <tahjib.leon399@gmail.com>
2022-05-31 02:41:18 +0600

    add deposite,withdraw & balance sections

--- js/bank.js
+++ js/bank.js
@@ -1,10 +1,21 @@
 document.getElementById('login-submit').addEventListener('click', function () {
-    // get user email
+    // ! get user email
     const emailValue = document.getElementById('user-email');
     const userEmail = emailValue.value;
-    console.log(userEmail);
-    // get user password
+    // console.log(userEmail);
+    // ! get user password
     const passValue = document.getElementById('user-pass');
     const userPass = passValue.value;
-    console.log(userPass);
+    // console.log(userPass);
+
+    // ! applying condition to see it is valid customer or not
+
+    if (userEmail == 'tahjib@gmail.com' && userPass == 'leon') {
+        // console.log('valid user');
+        window.location.href = 'banking.html';
+    }
+
+    else {
+        console.log('Invalid user');
+    }
 })
